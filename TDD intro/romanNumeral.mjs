af08bb4263946d3c77c9f5b32779fb0d90c4e0c1
Oskar <oskardb@uia.no>
2025-01-21 17:34:55 +0100

    Extend the roman numerals converter to 1000

--- TDD intro/romanNumeral.mjs
+++ TDD intro/romanNumeral.mjs
@@ -1,12 +1,56 @@
 // Decimal to Roman Numeral converter 
 
 function romanNumeralConverter(nInt) {
+    if (nInt <= 0 || nInt > 1000) {
+        throw new Error("Input has to be between 1-1000.");
+    }
 
     let output = "";
+
     while (nInt > 0) {
         let diff = 0;
 
-        if (nInt >= 5) {
+        if (nInt >= 1000) {
+            output += "M";
+            diff = 1000;
+        }
+        else if (nInt >= 900) {
+            output += "CM";
+            diff = 900;
+        }
+        else if (nInt >= 500) {
+            output += "D";
+            diff = 500;
+        }
+        else if (nInt >= 400) {
+            output += "CD";
+            diff = 400;
+        }
+        else if (nInt >= 100) {
+            output += "C";
+            diff = 100;
+        }
+        else if (nInt >= 90) {
+            output += "XC";
+            diff = 90;
+        }
+        else if (nInt >= 50) {
+            output += "L";
+            diff = 50;
+        }
+        else if (nInt >= 40) {
+            output += "XL";
+            diff = 40;
+        }
+        else if (nInt >= 10) {
+            output += "X";
+            diff = 10;
+        }
+        else if (nInt >= 9) {
+            output += "IX";
+            diff = 9;
+        }
+        else if (nInt >= 5) {
             output += "V";
             diff = 5;
         }
@@ -25,5 +69,4 @@ function romanNumeralConverter(nInt) {
     return output;
 
 }
-//test
 export default romanNumeralConverter
