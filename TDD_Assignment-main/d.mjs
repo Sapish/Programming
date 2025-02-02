4bef299fdf68d262ecdb61ec0a9936ec47336ddb
Oskar <oskardb@uia.no>
2025-02-02 17:32:19 +0100

    test for edge cases

--- TDD_Assignment-main/d.mjs
+++ TDD_Assignment-main/d.mjs
@@ -21,7 +21,6 @@ import test from "./test.mjs";
 */
 
 //#region function -----------------------------------------------------------------
-// Write your function her.
 
 function guessNumber(target, guess) {
     if (Number.isInteger(target) === false || Number.isInteger(guess) === false) {
@@ -41,22 +40,21 @@ if (guess < target) {
 
 //#region Tests --------------------------------------------------------------------
 
-// Basic cases
 test.isEqual(guessNumber(10, 5), "Too low", "If target is 10 and guess is 5, return 'Too low'");
 test.isEqual(guessNumber(10, 15), "Too high", "If target is 10 and guess is 15, return 'Too high'");
 test.isEqual(guessNumber(10, 10), "Correct!", "If target is 10 and guess is 10, return 'Correct!'");
 
-// Invalid inputs
-test.isEqual(guessNumber(10, 6.5), "If the guess is not an integer, then return null");
-test.isEqual(guessNumber(10.5, 6), "If the target is not an integer, then return null");
-test.isEqual(guessNumber(NaN, 6.5), "If the target is not a number, then return null");
-test.isEqual(guessNumber(10, NaN), "If the guess is not a number, then return null");
-test.isEqual(guessNumber(null, 6.5), "If the target is null, then return null");
-test.isEqual(guessNumber(10, null), "If the guess is null, then return null");
-test.isEqual(guessNumber("10", 6.5), "If the target is a string, then return null");
-test.isEqual(guessNumber(10, "6.5"), "If the guess is a string, then return null");
+test.isEqual(guessNumber(10, 6.5), null, "If the guess is not an integer, then return null");
+test.isEqual(guessNumber(10.5, 6), null, "If the target is not an integer, then return null");
+test.isEqual(guessNumber(NaN, 6.5), null, "If the target is not a number, then return null");
+test.isEqual(guessNumber(10, NaN), null, "If the guess is not a number, then return null");
+test.isEqual(guessNumber(null, 6.5), null, "If the target is null, then return null");
+test.isEqual(guessNumber(10, null), null, "If the guess is null, then return null");
+test.isEqual(guessNumber("10", 6.5), null, "If the target is a string, then return null");
+test.isEqual(guessNumber(10, "6.5"), null, "If the guess is a string, then return null");
 
-// Edge cases
-
+test.isEqual(guessNumber(9, 9), "Correct!", "Target and guess is the same number");
+test.isEqual(guessNumber(9, 12), "Too High", "Target is lower than the guess");
+test.isEqual(guessNumber(9, 6), "Too Low", "Target is higher than the guess");
 
 //#endregion
