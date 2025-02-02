5ff2f9372a48696f965382d7ace8a9a8320adcc4
Oskar <oskardb@uia.no>
2025-02-02 17:56:01 +0100

    removed some comments

--- TDD_Assignment-main/c.mjs
+++ TDD_Assignment-main/c.mjs
@@ -35,7 +35,6 @@ for (let i = 3; i <= n; i++) {
 //#region Tests --------------------------------------------------------------------
 const tests = test("Sum function");
 
-// Basic cases
 tests.isEqual(sequence(0), 0, "For n = 0, the function should return 0");
 tests.isEqual(sequence(1), 1, "For n = 1, the function should return 1");
 tests.isEqual(sequence(2), 1, "For n = 2, the function should return 1");
@@ -45,10 +44,8 @@ tests.isEqual(sequence(5), 5, "For n = 5, the function should return 5");
 tests.isEqual(sequence(6), 8, "For n = 6, the function should return 8");
 tests.isEqual(sequence(10), 55, "For n = 10, the function should return 55");
 
-// Large input
 tests.isEqual(sequence(20), 6765, "For n = 20, the function should return 6765");
 
-// Edge cases
 tests.isEqual(sequence(-1), null, "Negative input should return null");
 tests.isEqual(sequence(1.5), null, "Non-integer input should return null");
 tests.isEqual(sequence("5"), null, "String input should return null");
