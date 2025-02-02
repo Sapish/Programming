0e049752779787fe40421e635bbc22f33b7551d4
Oskar <oskardb@uia.no>
2025-02-02 17:47:56 +0100

    defined tests (Facepalm)

--- TDD_Assignment-main/d.mjs
+++ TDD_Assignment-main/d.mjs
@@ -39,22 +39,23 @@ if (guess < target) {
 //#endregion
 
 //#region Tests --------------------------------------------------------------------
+const tests = test("Guess number function")
 
-test.isEqual(guessNumber(10, 5), "Too low", "If target is 10 and guess is 5, return 'Too low'");
-test.isEqual(guessNumber(10, 15), "Too high", "If target is 10 and guess is 15, return 'Too high'");
-test.isEqual(guessNumber(10, 10), "Correct!", "If target is 10 and guess is 10, return 'Correct!'");
+tests.isEqual(guessNumber(10, 5), "Too low", "If target is 10 and guess is 5, return 'Too low'");
+tests.isEqual(guessNumber(10, 15), "Too high", "If target is 10 and guess is 15, return 'Too high'");
+tests.isEqual(guessNumber(10, 10), "Correct!", "If target is 10 and guess is 10, return 'Correct!'");
 
-test.isEqual(guessNumber(10, 6.5), null, "If the guess is not an integer, then return null");
-test.isEqual(guessNumber(10.5, 6), null, "If the target is not an integer, then return null");
-test.isEqual(guessNumber(NaN, 6.5), null, "If the target is not a number, then return null");
-test.isEqual(guessNumber(10, NaN), null, "If the guess is not a number, then return null");
-test.isEqual(guessNumber(null, 6.5), null, "If the target is null, then return null");
-test.isEqual(guessNumber(10, null), null, "If the guess is null, then return null");
-test.isEqual(guessNumber("10", 6.5), null, "If the target is a string, then return null");
-test.isEqual(guessNumber(10, "6.5"), null, "If the guess is a string, then return null");
+tests.isEqual(guessNumber(10, 6.5), null, "If the guess is not an integer, then return null");
+tests.isEqual(guessNumber(10.5, 6), null, "If the target is not an integer, then return null");
+tests.isEqual(guessNumber(NaN, 6.5), null, "If the target is not a number, then return null");
+tests.isEqual(guessNumber(10, NaN), null, "If the guess is not a number, then return null");
+tests.isEqual(guessNumber(null, 6.5), null, "If the target is null, then return null");
+tests.isEqual(guessNumber(10, null), null, "If the guess is null, then return null");
+tests.isEqual(guessNumber("10", 6.5), null, "If the target is a string, then return null");
+tests.isEqual(guessNumber(10, "6.5"), null, "If the guess is a string, then return null");
 
-test.isEqual(guessNumber(9, 9), "Correct!", "Target and guess is the same number");
-test.isEqual(guessNumber(9, 12), "Too High", "Target is lower than the guess");
-test.isEqual(guessNumber(9, 6), "Too Low", "Target is higher than the guess");
+tests.isEqual(guessNumber(9, 9), "Correct!", "Target and guess is the same number");
+tests.isEqual(guessNumber(9, 12), "Too High", "Target is lower than the guess");
+tests.isEqual(guessNumber(9, 6), "Too Low", "Target is higher than the guess");
 
 //#endregion
